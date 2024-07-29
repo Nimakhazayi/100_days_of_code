3fe6d964a62b0662c49263a9818371e0c342370c
fluffy persimmon i mighttt? <104640737+Nimakhazayi@users.noreply.github.com>
2024-07-29 01:44:13 -0700

    Add files via upload

---
+++ Day 1/Band_Name_Generator.py
@@ -0,0 +1,7 @@
+
+print("\nWelcome to the Band Name Generator!")
+
+City_Name = input("What's the name of city you grew up in? \n")
+Pets_Name = input("What is your favourite pet's name? \n")
+
+print("Our suggested Band Name is " + City_Name + " " +  Pets_Name)
